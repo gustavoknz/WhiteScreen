1d7ea80c15b993d53b39dbea01949f747276e00a
Gustavo Kieling <gustavoknz@gmail.com>
2024-06-03 22:11:41 +0100

    *Bump app version *Rename app name to Blank *Update from dependencies

--- app/src/main/java/com/kieling/whitescreen/ui/MainActivity.kt
+++ app/src/main/java/com/kieling/whitescreen/ui/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.kieling.whitescreen.ui
 
-import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
-import android.view.WindowInsets
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
