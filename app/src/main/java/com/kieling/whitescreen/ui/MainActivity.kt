3a13173529bca015824f29c937ffa3937232bb9f
Gustavo Kieling <gustavoknz@gmail.com>
2024-08-15 17:04:01 +0100

    Prevent screen lock

--- app/src/main/java/com/kieling/whitescreen/ui/MainActivity.kt
+++ app/src/main/java/com/kieling/whitescreen/ui/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.kieling.whitescreen.ui
 
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat.getInsetsController
+import androidx.core.view.WindowInsetsCompat.Type.systemBars
 import com.kieling.whitescreen.ui.theme.WhiteScreenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        // Set fullscreen
+        enableEdgeToEdge()
+        val windowInsetsController = getInsetsController(window, window.decorView)
+        windowInsetsController.hide(systemBars())
+
+        // Avoid screen lock
+        window.addFlags(FLAG_KEEP_SCREEN_ON)
 
         setContent { AddMainScreen() }
     }
